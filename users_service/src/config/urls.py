a00011d1a60fe77c830c1a8a078a2da4b1f8a563
o4codes <oforkansi.shadrach@gmail.com>
2023-11-29 23:01:39 +0100

    added swagger docs url

--- users_service/src/config/urls.py
+++ users_service/src/config/urls.py
@@ -16,9 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/docs/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/docs/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
     path("api/v1/auth/", include("src.apps.auth.urls")),
     path("api/v1/users/", include("src.apps.users.urls")),
 ]
